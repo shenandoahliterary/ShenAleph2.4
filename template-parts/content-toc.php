a84bd09fc3e9485c0c1297ea3a3890a2d504dddc
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2024-03-29 13:55:50 -0400

    Update content-toc.php

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -37,6 +37,8 @@
                     &nbsp;▴&nbsp;
                     <a href="#fiction">Fiction</a>
                     &nbsp;▴&nbsp;
+                    <a href="#nonfiction">Non-Fiction</a>
+                    &nbsp;▴&nbsp;
                     <a href="#poetry">Poetry</a>
                     &nbsp;▴&nbsp;
                     <a href="#comics">Comics</a>
@@ -155,7 +157,87 @@ echo "$my_custom_field[0]";
 
                         <a id="micro"></a>
             <span class="text-center p-section-break">▴&nbsp;▴&nbsp;▴</span>
+            <div class="row"> <!-- opens row for second category -->
 
+<div class="TOC-column">
+    <h3>Fiction</h3>
+</div>
+</div>
+<div class="row">
+
+                <?php
+                remove_all_filters('posts_orderby');
+                $fiction_args = array(
+                    'category_name' => 'fiction',
+                    'order' => 'ASC',
+                    'meta_key' => 'TOC_order',
+                    'orderby' => 'meta_value_num',
+                    'meta_type' => 'NUMERIC',
+                    'nopaging' => 'true',
+
+                );
+                $fiction_loop = new WP_Query($fiction_args);
+                    $authornames = array();
+
+                while ($fiction_loop->have_posts()) : $fiction_loop->the_post();
+                    $this_author= get_post_meta($post->ID, 'author_lastname', true);
+                    $this_author_id =get_the_author_meta('ID');
+                    $authornames[$this_author_id] = $this_author;
+
+                //print statement of title and author just below worked but put each work and author separately
+                ?>
+
+                <?php
+                        endwhile;
+
+                //below groups posts by author
+
+                    foreach ($authornames as $author_id=>$author_lastname) {
+                            $args = array(
+                        'category_name' => 'fiction',
+                        'author' => $author_id,
+                        'orderby' => 'date',
+                        'order' => 'asc',
+                        'nopaging' => 'true'
+                        );
+                ?>
+                <?php
+                        $fiction_loop_single = new WP_Query($args);
+
+                        $i = 0;
+                        //open paragraph for title(s)/author
+                        echo "<p>";
+                        while ($fiction_loop_single->have_posts()) :
+                            $fiction_loop_single->the_post();
+                        //for each author, print title, title, author
+                ?>
+
+                <a href="<?php the_permalink(); ?>">
+
+                <?php the_title(); ?>
+
+                </a><br />
+
+                <?php
+                        $i++;
+                        endwhile;
+                        //print author outside of the loop
+                ?>
+                        <span class="author_name"><?php the_author(); ?> </span><br />
+                        <?php
+//add translator byline
+$custom_fields = get_post_custom();
+
+$my_custom_field = $custom_fields['translator_byline'];
+echo "$my_custom_field[0]";
+
+?>
+                <?php
+                            wp_reset_postdata();
+                        }
+                ?>
+
+            </div>
   
 
 
